44a25fac98e0d13e2b635ba4523fb6a87b6a605e
Heloiselin <heloiselin101@gmail.com>
2022-11-08 12:03:37 +0800

    修改目錄

---
+++ week07/week07-4.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+    printf("瞷计%d\n", n%10);
+    n=n/10;
+
+}
---
+++ week08/week08-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int sum=0;
+    for(int i=0; i<5; i++)
+    {
+        scanf("%d", &n);
+        sum += n;
+    }
+    printf("Á`©M¬O¡G%d", sum);
+}
+
