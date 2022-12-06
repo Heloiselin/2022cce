95f00789f44a4cb35dd66f9c263d8ed754ed9884
Heloiselin <heloiselin101@gmail.com>
2022-12-06 11:54:35 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int sum(int a,  int b);
+int main()
+{///在用之前要先宣告declare或定義define
+    int ans = sum(2, 3);///使用 呼叫
+    printf("ans: %d\n", ans);
+}
+int sum(int a, int b)///定義define
+{
+    return a+b;
+}///回傳
